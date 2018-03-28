785e6c477030844e85ff3fb401f13dcaeedd0852
Hofschneider <Hos@gmail.com>
2018-03-28 17:27:23 +0200

    Hofschneider: Product name

---
+++ Bank/sell_Hofschneider/sell_Hofschneider.java
@@ -0,0 +1,15 @@
+public class Product{
+	private String name;
+	private int price;
+	
+public Product(){
+	name= "Coca Cola";
+	
+	
+}
+
+public void setProduct(String product){
+	name = Product;
+}
+
+}
